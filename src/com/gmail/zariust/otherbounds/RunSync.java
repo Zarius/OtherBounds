d80ccea1e7b1e59806e6b89fc19520dd810fce15
Zarius <zariust@gmail.com>
2013-03-25 02:13:17 +1000

    Minor: iterate over copy of DamageList to avoid possible (not yet seen) ConcurrentModification exception.

--- src/com/gmail/zariust/otherbounds/RunSync.java
+++ src/com/gmail/zariust/otherbounds/RunSync.java
@@ -1,7 +1,9 @@
 package com.gmail.zariust.otherbounds;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.entity.Player;
 
@@ -12,16 +14,19 @@ class RunSync implements Runnable {
     public void run() {
     	OtherBounds.logInfo("Sync run... damageList: "+OtherBounds.damageList.keySet().toString(), Verbosity.HIGHEST);
     	List<Player> removeList = new ArrayList<Player>();
+        Map<Player, Effects> iterateMap = new HashMap<Player, Effects>();
+    	iterateMap.putAll(OtherBounds.damageList);
     	
         // Check if player in list
-    	for (Player player : OtherBounds.damageList.keySet()) {
+    	for (Player player : iterateMap.keySet()) {
     		if (!player.isOnline() || player.isDead()) {
     			// add player to new list as we cannot remove from the
     			// damageList whilst iterating through it.
     			removeList.add(player);
     			continue;
     		}
-    		Effects effects = OtherBounds.damageList.get(player);
+    		Effects effects = iterateMap.get(player);
+    		
     		int damage = effects.damagePerCheck + effects.invertedDamagePerCheck;
     		if (damage > 0) { 
     			OtherBounds.logInfo("Damaging player ("+player.getName()+") for "+damage+" damage.", Verbosity.HIGHEST);
